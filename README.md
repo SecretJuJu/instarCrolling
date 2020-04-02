c7f29bfc5c0138725d24669723d7dfbf57f303b4
secretJuJu <tmdqh04033@gmail.com>
2020-04-02 23:43:57 +0900

    Updated for user convenience

---
+++ README.md
@@ -0,0 +1,29 @@
+# instar croller
+
+## Pre-execution Verification
+    
+    http://chromedriver.storage.googleapis.com/index.html
+    
+    Download the chrome driver for your version
+    
+## how to
+    Set the chrome driver in the environment variable
+    
+    or
+     
+    put the path of the chrome driver file in 'driverPath.txt'
+        ex) C:\\Users/mypc/downloads/
+
+###choose 1 or 2
+    
+    1 : find by tag
+    2 : find by account
+    
+### input keyword
+    
+==
+
+### wait
+
+    Wait for images to download
+
--- instarImages.py
+++ instarImages.py
@@ -5,19 +5,40 @@ from selenium import webdriver
 import time
 import urllib.request
 import threading
-
 import os
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
+dp = open('./driverPath.txt','r')
+driver_path = dp.readline()
 
 last_height=0
 new_height=0
 loading_end = False
 htmls = []
+num = 1
+links = []
+already_downloaded = []
+
+def download():
+    global links
+    global already_downloaded
+    global num
+    try:
+        while True:
+            for link in links:
+                if link in already_downloaded:
+                    continue
+                path = './images/' + keyword + '/' + str(num) + '.jpg'
+                print(link)
+                print(path)
+                urllib.request.urlretrieve(link, path)
+                num += 1
+                already_downloaded.append(link)
+            if loading_end:
+                break
+    except e as msg:
+        if len(links) != 0:
+            print(msg)
 
 def execute(driver):
     global loading_end
@@ -36,33 +57,42 @@ def execute(driver):
             ## more chance
             time.sleep(3)
             body.send_keys(Keys.PAGE_DOWN)
+            time.sleep(1)
+            body.send_keys(Keys.PAGE_DOWN)
             new_height = driver.execute_script("return document.body.scrollHeight")
             if(new_height == last_height):
                 print("loading is over")
-
                 loading_end = True
                 break
         last_height = new_height
 
 
+
 def crolling(baseUrl,keyword):
-    # baseUrl = 'https://www.instagram.com/explore/tags/'
-    # baseUrl = 'https://www.instagram.com/'
+    # baseUrl = 'https://www.instagram.com/explore/tags/'   # find by tags
+    # baseUrl = 'https://www.instagram.com/'                # find by account
     plusUrl = keyword
 
     url = baseUrl + quote_plus(plusUrl)
+    try :
+        driver = webdriver.Chrome()
+    except :
+        driver = webdriver.chrome(driver_path)
 
-    driver = webdriver.Chrome()
     driver.get(url)
 
     # infinite page down
     thread1 = threading.Thread(target=execute,args=(driver,))
     thread1.start()
-
+    thread2 = threading.Thread(target=download)
+    thread2.start()
     i = 1
 
-    links = []
+    global links
+    global already_downloaded
+
     while True:
+
         if i >= len(htmls):
             tmp = len(htmls)
             time.sleep(2)
@@ -74,19 +104,14 @@ def crolling(baseUrl,keyword):
         else :
             soup = BeautifulSoup(htmls[i], 'html.parser')
         try:
-            # posts = driver.find_elements_by_css_selector(".v1Nh3.kIKUG._bz0w a")
-            #posts = soup.select('.v1Nh3.kIKUG._bz0w a')
             imgs = soup.select('.v1Nh3.kIKUG._bz0w a img')
-            # print(len(posts))
-            # print(len(imgs))
-
             for src in imgs:
                 links.append(src['src'])
             print(str(i)+"번째 로딩 기록...")
             i += 1
         except Exception as e:
-            print(str(e))
-            print("error.....")
+            print(e)
+            print(Exception)
             time.sleep(1)
 
 
@@ -103,7 +128,11 @@ while True:
         continue
 
 
-
+try:
+    if not(os.path.isdir(keyword)):
+        os.makedirs(os.path.join('./images/'+keyword))
+except OSError as e:
+    print("Failed to create directory!!!!!")
 
 if(choose == 1):
     baseUrl = 'https://www.instagram.com/explore/tags/'
@@ -113,19 +142,4 @@ else :
     baseUrl = 'https://www.instagram.com/'
     links = crolling(baseUrl=baseUrl, keyword=keyword)
 
-try:
-    if not(os.path.isdir(keyword)):
-        os.makedirs(os.path.join('./images/'+keyword))
-except OSError as e:
-    if e.errno != e.EEXIST:
-        print("Failed to create directory!!!!!")
-        raise
-
-num = 1
-for link in links:
-    path = './images/' + keyword + '/' + str(num) + '.jpg'
-    print(link)
-    print(path)
-    urllib.request.urlretrieve(link,path)
-    num += 1
 print(len(links))
